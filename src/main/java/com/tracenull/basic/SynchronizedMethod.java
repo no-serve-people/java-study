4ea65630cd281de41c2f2327fb49208f7653f91c
ixingjue <ixingjue@qq.com>
2020-08-01 09:31:17 +0800

    test

---
+++ src/main/java/com/tracenull/basic/SynchronizedMethod.java
@@ -0,0 +1,36 @@
+package com.tracenull.basic;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.IntStream;
+
+
+public class SynchronizedMethod {
+    public static void main(String[] args) throws InterruptedException {
+        ExecutorService service = Executors.newFixedThreadPool(3);
+        Test summation = new Test();
+        IntStream.range(0, 1000).forEach(count -> service.submit(summation::calculate));
+
+        service.awaitTermination(1000, TimeUnit.MILLISECONDS);
+        System.out.println(summation.getSum());
+    }
+}
+
+class Test {
+    private int sum;
+
+    public int getSum() {
+        return sum;
+    }
+
+    public void setSum(int sum) {
+        this.sum = sum;
+    }
+
+    public void calculate() {
+        setSum(getSum() + 1);
+    }
+
+}
+
