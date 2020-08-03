e559489eff2107678716a04c4a502224e9556aa1
ixingjue <ixingjue@qq.com>
2020-08-03 20:02:46 +0800

    FinalizerTest

---
+++ src/main/java/com/tracenull/basic/FinalizerTest.java
@@ -0,0 +1,30 @@
+package com.tracenull.basic;
+
+import sun.nio.cs.US_ASCII;
+
+class User {
+    public static User user = null;
+
+    @Override
+    protected void finalize() throws Throwable {
+        System.out.println("User-->finalize()");
+        user = this;
+    }
+}
+
+public class FinalizerTest {
+
+    public static void main(String[] args) throws InterruptedException {
+        User user = new User();
+        user=null;
+        System.gc();
+        Thread.sleep(1000);
+
+        user=User.user;
+        System.out.println(user != null);//true
+        user = null;
+        System.gc();
+        Thread.sleep(1000);
+        System.out.println(user != null);//false
+    }
+}
