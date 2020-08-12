79d5d13063087307043fde878d745ae025810d9f
ixingjue <ixingjue@qq.com>
2020-08-13 07:35:30 +0800

    Condition

---
+++ src/main/java/com/tracenull/condition/TaskQueueTest.java
@@ -0,0 +1,38 @@
+package com.tracenull.condition;
+
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class TaskQueueTest {
+    private final Lock lock = new ReentrantLock();
+    private final Condition condition = lock.newCondition();
+
+    private Queue<String> queue = new LinkedList<>();
+
+    public void addTask(String s) {
+        lock.lock();
+        try {
+            queue.add(s);
+            condition.signalAll();
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public String getTask() throws InterruptedException {
+        lock.lock();
+        try {
+            while (queue.isEmpty()) {
+                condition.await();
+            }
+            return queue.remove();
+        } finally {
+            lock.unlock();
+        }
+
+    }
+}
