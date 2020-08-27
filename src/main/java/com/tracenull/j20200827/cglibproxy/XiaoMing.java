7dbb19a1a486aa41d8c3537de7a9ed38a0773bc8
ixingjue <ixingjue@qq.com>
2020-08-27 14:51:59 +0800

    cglib动态代理

---
+++ src/main/java/com/tracenull/j20200827/cglibproxy/XiaoMing.java
@@ -0,0 +1,11 @@
+package com.tracenull.j20200827.cglibproxy;
+
+public class XiaoMing {
+    public static void main(String[] args) {
+        SellProxyFactory sellProxyFactory = new SellProxyFactory();
+        // 获取一个代理类
+//        SellPerfumeFactory proxyInstance = (SellPerfumeFactory) sellProxyFactory.getProxyInstance(new SellPerfumeFactory());
+//         创建代理类
+//        proxyInstance.sellPerfume(1999.99);
+    }
+}
