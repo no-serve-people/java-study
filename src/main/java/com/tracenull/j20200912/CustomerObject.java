4d76c4b95261d8010d5813b2a6084a5c6222a6cc
ixingjue <ixingjue@qq.com>
2020-09-12 16:36:08 +0800

    ClassLoaderDemo

---
+++ src/main/java/com/tracenull/j20200912/CustomerObject.java
@@ -0,0 +1,11 @@
+package com.tracenull.j20200912;
+
+/**
+ * https://mp.weixin.qq.com/s?__biz=MzAxNjQwOTU0Mg==&mid=2247484238&idx=1&sn=356504dc8b4d9c7e2fd1add25a34c6b5&chksm=9bf475e7ac83fcf18134ea60318c3c0a1609c6efb81d6b25972d7d64898756c19e9f76bb83a8&scene=178#rd
+ */
+public class CustomerObject {
+    @Override
+    public String toString() {
+        return "I am CustomerObject";
+    }
+}
