e6d35d84d30dfcde3fa0e3f5ee72533361bb086b
ixingjue <ixingjue@qq.com>
2020-10-01 08:47:46 +0800

    欢度国庆

---
+++ src/main/java/com/tracenull/j20201001/guoqing.java
@@ -0,0 +1,18 @@
+package com.tracenull.j20201001;
+
+/**
+ * https://mp.weixin.qq.com/s/vUSuF4mvnlihqsV-08f7rw
+ */
+public class guoqing {
+    public static void main(String[] args) {
+        String 欢 = "21152";
+        String 度 = "29677";
+        String 国 = "24555";
+        String 庆 = "20048";
+
+        System.err.print((char) Integer.parseInt(欢));
+        System.err.print((char) Integer.parseInt(度));
+        System.err.print((char) Integer.parseInt(国));
+        System.err.print((char) Integer.parseInt(庆));
+    }
+}
