3396ad6fdfe8985d36cd45f505cd989cfe4d8b65
ixingjue <ixingjue@qq.com>
2020-10-02 09:15:39 +0800

    比AtomicLong更优秀的LongAdder确定不来了解一下吗？

---
+++ src/main/java/com/tracenull/j20201002/AtomicLongAdderTest.java
@@ -0,0 +1,74 @@
+package com.tracenull.j20201002;
+
+import com.google.common.collect.Lists;
+
+import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.concurrent.atomic.LongAdder;
+
+/**
+ * 比AtomicLong更优秀的LongAdder确定不来了解一下吗？
+ * https://mp.weixin.qq.com/s/rJAIoZLe9lnEcTj3SmgIZw
+ */
+public class AtomicLongAdderTest {
+    public static void main(String[] args) throws Exception {
+        testAtomicLongAdder(1, 10000000);
+        testAtomicLongAdder(10, 10000000);
+        testAtomicLongAdder(100, 10000000);
+    }
+
+    static void testAtomicLongAdder(int threadCount, int times) throws Exception {
+        System.out.println("threadCount: " + threadCount + ", times: " + times);
+        long start = System.currentTimeMillis();
+        testLongAdder(threadCount, times);
+        System.out.println("LongAdder 耗时：" + (System.currentTimeMillis() - start) + "ms");
+        System.out.println("threadCount: " + threadCount + ", times: " + times);
+        long atomicStart = System.currentTimeMillis();
+        testAtomicLong(threadCount, times);
+        System.out.println("AtomicLong 耗时：" + (System.currentTimeMillis() - atomicStart) + "ms");
+        System.out.println("----------------------------------------");
+    }
+
+    static void testAtomicLong(int threadCount, int times) throws Exception {
+        AtomicLong atomicLong = new AtomicLong();
+        List<Thread> list = Lists.newArrayList();
+        for (int i = 0; i < threadCount; i++) {
+            list.add(new Thread(() -> {
+                for (int j = 0; j < times; j++) {
+                    atomicLong.incrementAndGet();
+                }
+            }));
+        }
+
+        for (Thread thread : list) {
+            thread.start();
+        }
+
+        for (Thread thread : list) {
+            thread.join();
+        }
+
+        System.out.println("AtomicLong value is : " + atomicLong.get());
+    }
+
+    static void testLongAdder(int threadCount, int times) throws Exception {
+        LongAdder longAdder = new LongAdder();
+        List<Thread> list = Lists.newArrayList();
+        for (int i = 0; i < threadCount; i++) {
+            list.add(new Thread(() -> {
+                for (int i1 = 0; i1 < times; i1++) {
+                    longAdder.increment();
+                }
+            }));
+        }
+
+        for (Thread thread : list) {
+            thread.start();
+        }
+
+        for (Thread thread : list) {
+            thread.join();
+        }
+        System.out.println("LongAdder value is : " + longAdder.longValue());
+    }
+}
