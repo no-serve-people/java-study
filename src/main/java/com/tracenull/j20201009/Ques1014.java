b4d136fc921b23d582455bb4cae7be77e555a269
ixingjue <ixingjue@qq.com>
2020-10-09 20:04:22 +0800

    Arrays.asList的坑

---
+++ src/main/java/com/tracenull/j20201009/Ques1014.java
@@ -0,0 +1,16 @@
+package com.tracenull.j20201009;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * https://mp.weixin.qq.com/s/Af2ZW76Jy7YxZX3ZnOUP0w
+ */
+public class Ques1014 {
+    public static void main(String[] args) {
+        int[] data = {1, 2, 3};
+        List list = Arrays.asList(data);
+//        List list = Arrays.asList(1, 2, 3);
+        System.out.println("列表的元素数量是：" + list.size());
+    }
+}
