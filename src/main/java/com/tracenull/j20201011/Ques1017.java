8b61bf1b63bdc634f1c7b70c2b03c57593ba8ab2
ixingjue <ixingjue@qq.com>
2020-10-11 12:07:20 +0800

    Vector和ArrayList

---
+++ src/main/java/com/tracenull/j20201011/Ques1017.java
@@ -0,0 +1,17 @@
+package com.tracenull.j20201011;
+
+import java.util.ArrayList;
+import java.util.Vector;
+
+/**
+ * https://mp.weixin.qq.com/s/9C-RsTGiGhKJBXr2B5W6CQ
+ */
+public class Ques1017 {
+    public static void main(String[] args) {
+        ArrayList<String> str01 = new ArrayList<String>();
+        str01.add("123");
+        Vector<String> str02 = new Vector<String>();
+        str02.add("123");
+        System.out.println(str01.equals(str02));
+    }
+}
