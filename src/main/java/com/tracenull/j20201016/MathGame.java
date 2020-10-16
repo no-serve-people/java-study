bf79d58a31563cb7843d1881099271227ce78f45
ixingjue <ixingjue@qq.com>
2020-10-16 11:04:46 +0800

    MathGame

---
+++ src/main/java/com/tracenull/j20201016/MathGame.java
@@ -0,0 +1,63 @@
+package com.tracenull.j20201016;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * https://github.com/alibaba/arthas/blob/master/demo/src/main/java/demo/MathGame.java
+ */
+public class MathGame {
+    private static Random random = new Random();
+
+    public int illegalArgumentCount = 0;
+
+    public static void main(String[] args) throws InterruptedException {
+        MathGame game = new MathGame();
+        while (true) {
+            game.run();
+            TimeUnit.SECONDS.sleep(1);
+        }
+    }
+
+    public void run() {
+        int number = random.nextInt() / 10000;
+        List<Integer> primeFactors = primeFactors(number);
+        print(number, primeFactors);
+    }
+
+    public static void print(int number, List<Integer> primeFactors) {
+        StringBuffer sb = new StringBuffer(number + "=");
+        for (Integer factor : primeFactors) {
+            sb.append(factor).append("*");
+        }
+        if (sb.charAt(sb.length() - 1) == '*') {
+            sb.deleteCharAt(sb.length() - 1);
+        }
+
+        System.out.println(sb);
+    }
+
+    public List<Integer> primeFactors(int number) {
+        if (number < 2) {
+            illegalArgumentCount++;
+            throw new IllegalArgumentException("number is: " + number + ",need>2");
+        }
+
+        List<Integer> result = new ArrayList<Integer>();
+
+        int i = 2;
+        while (i < number) {
+            if (number % i == 0) {
+                result.add(i);
+                number = number / i;
+                i = 2;
+            } else {
+                i++;
+            }
+        }
+
+        return result;
+    }
+}
