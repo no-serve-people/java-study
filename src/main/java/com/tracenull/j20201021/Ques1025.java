b6c51fe70181481add1169be380521d9542a92c8
ixingjue <ixingjue@qq.com>
2020-10-21 21:41:35 +0800

    最短路径原则

---
+++ src/main/java/com/tracenull/j20201021/Ques1025.java
@@ -0,0 +1,15 @@
+package com.tracenull.j20201021;
+
+import static java.lang.Math.PI;
+
+/**
+ * 最短路径原则
+ * https://mp.weixin.qq.com/s/bOHKyFtyHhleqm7Uo_AMFg
+ */
+public class Ques1025 {
+    public final static int PI = 3;// 常量名与静态导入的相同
+
+    public static void main(String[] args) {
+        System.out.println("PI=" + PI);
+    }
+}
