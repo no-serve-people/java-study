842fe513e8c87f9c4c4601a7290d03498642b090
ixingjue <ixingjue@qq.com>
2021-01-28 16:15:21 +0800

    你心中最高大上最牛X的技术到底是什么

---
+++ src/main/java/com/tracenull/j20210128/GreetingServer.java
@@ -0,0 +1,56 @@
+package com.tracenull.j20210128;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketTimeoutException;
+
+/**
+ * 你心中最高大上最牛X的技术到底是什么
+ * https://segmentfault.com/a/1190000038782113
+ */
+public class GreetingServer extends Thread {
+    private ServerSocket serverSocket;
+
+    public GreetingServer(int port) throws IOException {
+        serverSocket = new ServerSocket(port);
+        serverSocket.setSoTimeout(100000000);
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            try {
+                System.out.println("等待远程连接，端口号为：" + serverSocket.getLocalPort() + "...");
+
+                Socket server = serverSocket.accept();
+                System.out.println("远程主机地址：" + server.getRemoteSocketAddress());
+
+                DataInputStream in = new DataInputStream(server.getInputStream());
+                System.out.println(in.readUTF());
+                DataOutputStream out = new DataOutputStream(server.getOutputStream());
+                out.writeUTF("谢谢连接我：" + server.getLocalSocketAddress() + "\nGoodbye!");
+                server.close();
+            } catch (SocketTimeoutException s) {
+                System.out.println("Socket timed out!");
+                break;
+            } catch (IOException e) {
+                e.printStackTrace();
+                break;
+            }
+
+        }
+    }
+
+    public static void main(String[] args) {
+        int port = Integer.parseInt(args[0]);
+        try {
+            Thread t = new GreetingServer(port);
+            t.start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
